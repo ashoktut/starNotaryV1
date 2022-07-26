22fb546bc1703b703ca616c84ac8bff52d7915f6
ASHOK <ashoktut.2@gmail.com>
2022-07-26 17:13:14 +0200

    starNotaryV1 with truffle test cases

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+// const ConvertLib = artifacts.require("ConvertLib");
+// const MetaCoin = artifacts.require("MetaCoin");
+
+// module.exports = function(deployer) {
+//   deployer.deploy(ConvertLib);
+//   deployer.link(ConvertLib, MetaCoin);
+//   deployer.deploy(MetaCoin);
+// };
+
+
+const starNotary = artifacts.require("../contracts/StarNotary.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(starNotary);
+};
